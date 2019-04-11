7b3b2d4a9d8ff34e76e1139e21c8fb169acf97c4
JuanraCM <juanras89s@gmail.com>
2019-04-11 14:56:38 +0100

    Added association class and stock restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'welcome#index'
+
   get 'my-portfolio',  :to => 'users#my_portfolio'
   get 'search-stocks', :to => 'stocks#search'
+
+  resources :user_stocks, :only => [:create]
 end
