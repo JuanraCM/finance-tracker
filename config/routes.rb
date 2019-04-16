fb9e74d2a04dad6d7991b4663042f2b8d9682964
JuanraCM <juanras89s@gmail.com>
2019-04-16 12:37:00 +0100

    Added friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'my-portfolio',  :to => 'users#my_portfolio'
   get 'search-stocks', :to => 'stocks#search'
+  get 'my-friends',    :to => 'users#my_friends' 
 
   resources :user_stocks, :only => [:create, :destroy]
 end
